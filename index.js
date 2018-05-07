b57ae8783ca4ff5961f69a46a260e855ce9b25e0
Ricardo Maltez <ricardomaltez@gmail.com>
2018-05-07 10:35:49 +0100

    Init

---
+++ index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const configs = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+const io = require('socket.io-client');
+const socket = io(configs.socketServerURL);
+
+const chokidar = require('chokidar');
+
+let yolo = require('node-yolo');
+
+let detector = new yolo("darknet-configs", "cfg/coco.data", "cfg/yolov3.cfg", "yolov3.weights");
+
+let watcher;
+socket.on('connect', () => {
+    console.log('Connected to socket server');
+    watcher = chokidar.watch(configs.folderToWatch, {
+        ignoreInitial: true,
+        awaitWriteFinish: true
+    });
+    watcher.on('add', (path) => {
+        console.log('File added');
+        detector.detect(path).then(data => {
+            console.log(data);
+            socket.emit('detection', data);
+        });
+    });
+});
+
+socket.on('disconnect', () => {
+    console.log('Disconnected from socket server');
+    watcher.close();
+});
+
+
+
+
+

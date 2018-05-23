eb17cc3f03724c83783a9afe13b5f6cc6aa868f5
rcaceiro <r.caceiro@hotmail.com>
2018-05-23 19:10:50 +0100

    Added reconnect

--- index.js
+++ index.js
@@ -8,31 +8,36 @@ const chokidar = require('chokidar');
 
 let yolo = require('node-yolo');
 
-let detector = new yolo("darknet-configs", "cfg/coco.data", "cfg/yolov3.cfg", "yolov3.weights");
+let detector = new yolo('darknet-configs', 'cfg/coco.data', 'cfg/yolov3.cfg', 'yolov3.weights');
 
 let watcher;
-socket.on('connect', () => {
-    console.log('Connected to socket server');
-    socket.emit('authenticate',{username:'yolo',password:'yolo'});
-     watcher = chokidar.watch(configs.folderToWatch, {
-        ignoreInitial: true,
-        awaitWriteFinish: true
-    });
-    watcher.on('add', (path) => {
-        console.log('File added');
-        detector.detect(path).then(data => {
-            console.log(data);
-            socket.emit('detection', data);
-        });
-    });
+socket.on('connect', () =>
+{
+ console.log('Connected to socket server');
+ socket.emit('authenticate', {username: 'yolo', password: 'yolo'});
+ watcher = chokidar.watch(configs.folderToWatch, {
+  ignoreInitial: true,
+  awaitWriteFinish: true
+ });
+ watcher.on('add', (path) =>
+ {
+  console.log('File added');
+  detector.detect(path).then(data =>
+  {
+   console.log(data);
+   socket.emit('detection', data);
+  });
+ });
 });
 
-socket.on('disconnect', () => {
-    console.log('Disconnected from socket server');
-    watcher.close();
+socket.on('reconnect', () =>
+{
+ console.log('Reconnected to socket server');
+ socket.emit('authenticate', {username: 'yolo', password: 'yolo'});
 });
 
-
-
-
-
+socket.on('disconnect', () =>
+{
+ console.log('Disconnected from socket server');
+ watcher.close();
+});

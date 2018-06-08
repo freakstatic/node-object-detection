d42d1de96bd54af52a51cc6ab7d8e0d9717d9667
Ricardo Maltez <ricardomaltez@gmail.com>
2018-06-08 10:38:47 +0100

    Updated auth

--- index.js
+++ index.js
@@ -2,37 +2,72 @@ const fs = require('fs');
 const configs = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 const io = require('socket.io-client');
-const socket = io(configs.socketServerURL);
+
 
 const chokidar = require('chokidar');
+const request = require('request');
 
 let yolo = require('node-yolo');
 
 let detector = new yolo("darknet-configs", "cfg/coco.data", "cfg/yolov3.cfg", "yolov3.weights");
 
 let watcher;
-socket.on('connect', () => {
-    console.log('Connected to socket server');
-    socket.emit('authenticate',{username:'yolo',password:'yolo'});
-     watcher = chokidar.watch(configs.folderToWatch, {
-        ignoreInitial: true,
-        awaitWriteFinish: true
-    });
-    watcher.on('add', (path) => {
-        console.log('File added');
-        detector.detect(path).then(data => {
-            console.log(data);
-            socket.emit('detection', data);
+
+
+getToken().then((token) => {
+    const socket = io(configs.server.url + ':' + configs.server.socketPort);
+    socket.on('connect', () => {
+        console.log('Connected to socket server');
+        socket.emit('authenticate', token);
+        watcher = chokidar.watch(configs.folderToWatch, {
+            ignoreInitial: true,
+
+        });
+        watcher.on('add', (path) => {
+            console.log('File added');
+            detector.detect(path).then(data => {
+                console.log(data);
+                let detection = {
+                    objects: data,
+                    imgUrl: path,
+                };
+                socket.emit('detection', detection);
+            });
         });
     });
+    socket.on('disconnect', () => {
+        console.log('Disconnected from socket server');
+        watcher.close();
+    });
+}).catch(() => {
+    console.error('Unable to get token from the server');
 });
 
-socket.on('disconnect', () => {
-    console.log('Disconnected from socket server');
-    watcher.close();
-});
 
 
 
+function getToken() {
+    return new Promise(((resolve, reject) => {
+        let options = {
+            url: configs.server.url + ':' + configs.server.httpPort + '/api/login',
+            'content-type': 'application/json',
+            auth: {
+                user: configs.username,
+                password: configs.password
+            },
+            method: 'POST',
+            json:true
+        };
 
+        request(options, function (err, res, body) {
+            if (err) {
+                console.error(err);
+                reject();
+            }
+            resolve(body.token);
+        })
+
+    }));
+
+}
 
